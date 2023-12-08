6f1a741f645b95462c35054100d9fd2543ee11ef
NamPham23 <152579684+NamPham23@users.noreply.github.com>
2023-12-08 10:12:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if image is not None:
   image = Image.open(image)
   st.image(image, caption='Test image')
   if st.button('Predict'):
-    image = image.resize((8*8*))
+    image = image.resize((8*8))
     vector = np.array(image.convert('L')).reshape(1, -1)
     #vector = np.array(image)
     lable = str(model.predict(model.predict(vector))[0])
