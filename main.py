1179627a1cabbed79aa2294e06e70d59b455bede
NamPham23 <152579684+NamPham23@users.noreply.github.com>
2023-12-08 10:11:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pickle as pkl
 from PIL import Image
 import numpy as np
 
-class_list = {'0': 'FeMale', '1': 'Male'}
+class_list = {'0': '0', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9'}
 st.title('HandWritten Degit Recognition')
 
 #image = Image.open('vi-names.png')
@@ -19,8 +19,9 @@ if image is not None:
   image = Image.open(image)
   st.image(image, caption='Test image')
   if st.button('Predict'):
-    image = image.resize((8*8*1, 1))
-    vector = np.array(image)
+    image = image.resize((8*8*))
+    vector = np.array(image.convert('L')).reshape(1, -1)
+    #vector = np.array(image)
     lable = str(model.predict(model.predict(vector))[0])
 
     st.header('Result')
